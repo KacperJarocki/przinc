c9b9516de0c83ad4f9f2a59a81049d050a9a5f9e
Kacper Jarocki <dev.kacperjarocki@proton.me>
2025-12-04 21:26:30 +0100

    base aks chat generated

--- infra/modules/aks/variables.tf
+++ infra/modules/aks/variables.tf
@@ -9,11 +9,23 @@ variable "name_components" {
 
 variable "configuration" {
   type = object({
-    config = optional(string)
+    node_vm_size     = optional(string, "Standard_B4ms")
+    node_count       = optional(number, 1)
+    enable_autoscale = optional(bool, false)
+    min_count        = optional(number, 1)
+    max_count        = optional(number, 3)
+    use_spot         = optional(bool, true)
+    spot_max_price   = optional(number, -1)
   })
 }
+
 variable "inputs" {
   type = object({
-    config = optional(string)
+    resource_group_name = string
+    location            = string
+    subnet_id           = string
+    log_analytics_id    = optional(string)
+    tags                = optional(map(string), {})
   })
 }
+
