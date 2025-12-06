1f8ab2f371f874d1ced7dcee1650d7b967386b83
NataliaJanda <55855610+NataliaJanda@users.noreply.github.com>
2025-12-06 12:55:49 +0100

    Feature/frontend (#6)
    * Initial aplication and database
    
    * qick fix
    
    * Angular front

--- Apps/app/main.py
+++ Apps/app/main.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 from auth import auth_bp
 from tickets import tickets_bp
 from admin import admin_bp
 
 app = Flask(__name__)
+CORS(app)
 
-# Rejestrujemy blueprinty
-app.register_blueprint(auth_bp)
-app.register_blueprint(tickets_bp)
-app.register_blueprint(admin_bp)
+app.register_blueprint(auth_bp, url_prefix="/api")
+app.register_blueprint(tickets_bp, url_prefix="/api")
+app.register_blueprint(admin_bp, url_prefiks="/api")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return jsonify({
+	"status": "OK",
+	"message": "Dziala"
+	})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
